34faafb40cfccc5f0d0152a22d39a39b41be3475
Juliana Allocati <juliana_allocati@hotmail.com>
2023-04-25 18:24:07 -0300

    Realice cambios esteticos

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./ItemDetail.css";
 
 export const ItemDetail = ({ product }) => {
   return (
-    <div>
+    <div className="cardDetail">
       <img src={product.img} alt="" />
       <h2>{product.producto}</h2>
-      <h2>{product.precio}</h2>
+      <h3>{product.precio}</h3>
       <Link to="/">Volver al home</Link>
     </div>
   );
